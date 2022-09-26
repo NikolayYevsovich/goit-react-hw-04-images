44d5e5a325a6a11a54b3d931e58ce5e0aa808a02
NikolayYevsovich <yevsovich.nick@gmail.com>
2022-09-26 17:10:40 +0300

    fix

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import Modal from '../Modal/Modal';
 import css from './ImageGalleryItem.module.css';
 
-// export const ImageGalleryItem = ({ webformatURL, largeImageURL, alt }) => {
-//   return (
-//     <li className={css.ImageGalleryItem}>
-//       <img className={css.ImageGalleryItemImage} src={webformatURL} alt={alt} />
-//     </li>
-//   );
-// };
-
 export default class ImageGalleryItem extends Component {
   state = {
     showModal: false,
